993625393e4e9351e4c96b2561aa81a4c8d4cc9f
John Nguyen <78591393+jnguyen1111@users.noreply.github.com>
2021-12-19 19:17:21 -0600

    Add files via upload

---
+++ source code/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MemeGenerator from './MemeGenerator';
+import Header from './Header';
+
+//returns the application and its component section
+function App() {
+  return (
+    <div>
+        <Header />
+        <MemeGenerator />
+    </div>
+  )
+}
+
+export default App;
---
+++ source code/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//render the application
+ReactDOM.render(<App />,document.getElementById('root'));
+
